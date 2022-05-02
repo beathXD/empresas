4cb12e2e1dd8c970de9475825e3bc1ae24cdc6e4
PicholaCesar <100111940+PicholaCesar@users.noreply.github.com>
2022-05-02 16:40:06 -0600

    vistas Control Empresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { EmpresasComponent } from './componentes/empresas/empresas.component';
+import { MenuvarComponent } from './componentes/menuvar/menuvar.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ProductosComponent } from './componentes/productos/productos.component';
+import { SucursalesComponent } from './componentes/sucursales/sucursales.component';
+import { SucursalIndiComponent } from './componentes/sucursal-indi/sucursal-indi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    EmpresasComponent,
+    MenuvarComponent,
+    InicioComponent,
+    ProductosComponent,
+    SucursalesComponent,
+    SucursalIndiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
